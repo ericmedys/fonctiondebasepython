51f6745fa72688dd745494e7e4283feac8f08caa
sabrina yaker <sabrina.yak91@gmail.com>
2021-07-20 19:28:30 +0200

    write file

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 
 def readfile():
     with open('file.txt', 'r') as f:
-        print(f.read())
+        result = f.read()
+        print(result)
+        return result
 
+
+def writefile():
+    with open('file_2.txt', 'w') as f:
+        f.write(readfile())
 
 
 if __name__ == '__main__':
-    readfile()
+    #readfile()
+    writefile()
+
 
 
